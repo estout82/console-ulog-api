35a846c7138bc15bada4386937edb3d0fb2457da
BaysideChurch <estout82@gmail.com>
2021-11-26 16:15:11 -0800

    added animation tests

--- test/models/animation_test.rb
+++ test/models/animation_test.rb
@@ -1,7 +1,56 @@
 require "test_helper"
 
 class AnimationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "a valid animation saves" do
+        animation = new_valid_animation
+        assert animation.save
+    end
+
+    test "must have name" do
+        animation = new_valid_animation
+        animation.name = nil
+        assert_not animation.save
+    end
+
+    test "must have slug" do
+        animation = new_valid_animation
+        animation.slug = nil
+        assert_not animation.save
+    end
+
+    test "slug must not contain spaces" do
+        # no spaces
+        # no unacceptable chars
+
+        animation = new_valid_animation
+        animation.slug = "a slug"
+        assert_not animation.save
+    end
+
+    test "slug must be unique" do
+        animation = new_valid_animation
+        animation.slug = "test"
+        assert_not animation.save
+    end
+
+    test "secret is hashed" do
+        animation = new_valid_animation
+        assert animation.save
+        assert_not_equal animation.secret, "1234"
+    end
+
+    test "frames relationship works" do
+        animation = animations(:one)
+        assert animation.frames.count > 1
+    end
+
+    private
+
+    def new_valid_animation
+        animation = Animation.new
+        animation.name = "Test Name"
+        animation.slug = "test-name"
+        animation.secret = "1234"
+        return animation
+    end
 end
